8e9fc30da1c5b8b384df818614104ae988201fd0
Balázs Richer <richer.balazs@gmail.com>
2018-11-24 14:23:56 +0100

    Intorduce the struct InputBuffer.

--- bdb.c
+++ bdb.c
@@ -14,3 +14,31 @@ int main (int argc, char* argv[]) {
     }
   }
 }
+
+typedef struct InputBuffer {
+  char* buffer;
+  size_t = buffer_length;
+  ssize_t = input_length;
+}
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void print_prompt() {
+  printf("|Talktome|>");
+}
+
+/*
+ * lineptr : pointer to the variable which points to the
+ *           buffer containing the read line.
+ * n: a pointer to the variable to save the size of allocated buffer.
+ * stream: the input stream to read from (stdin)
+ * return value: the number of bytes read.
+ */
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
