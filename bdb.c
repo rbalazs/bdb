2520fbe60de02395131fc4c0c4d84d9a15cc8a0e
Balázs Richer <richer.balazs@gmail.com>
2018-11-24 14:36:34 +0100

    Working REPL.

--- bdb.c
+++ bdb.c
@@ -1,25 +1,14 @@
-/*
- * First step: lets create a simple read-execute-print loop (REPL).
- */
-int main (int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer();
-  while (true) {
-    print_prompt();
-    read_input(input_buffer);
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    if (strcmp(input_buffer->buffer, '.die') == 0) {
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Command not found.\n", );
-    }
-  }
-}
-
-typedef struct InputBuffer {
+typedef struct InputBuffer_ty {
   char* buffer;
-  size_t = buffer_length;
-  ssize_t = input_length;
-}
+  size_t buffer_length;
+  ssize_t input_length;
+};
+typedef struct InputBuffer_t InputBuffer;
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
@@ -30,15 +19,40 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
-void print_prompt() {
-  printf("|Talktome|>");
+void print_prompt() { printf("|tAlkToME|> "); }
+
+void read_input(InputBuffer* input_buffer) {
+  /*
+   * ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+   * lineptr : pointer to the variable which points to the
+   *           buffer containing the read line.
+   * n: a pointer to the variable to save the size of allocated buffer.
+   * stream: the input stream to read from (stdin)
+   * return value: the number of bytes read.
+   */
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Faliora\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-/*
- * lineptr : pointer to the variable which points to the
- *           buffer containing the read line.
- * n: a pointer to the variable to save the size of allocated buffer.
- * stream: the input stream to read from (stdin)
- * return value: the number of bytes read.
- */
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+int main(int argc, char* argv[]) {
+  InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".die") == 0) {
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Command not found '%s'.\n", input_buffer->buffer);
+    }
+  }
+}
