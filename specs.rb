4689cbfd847438da5e41bfb04fd220010b8b2164
Balázs Richer <richer.balazs@gmail.com>
2018-12-01 14:44:42 +0100

    Test for writing table to treshold.

--- specs.rb
+++ specs.rb
@@ -30,4 +30,13 @@ describe 'database' do
       "bdb > ",
     ])
   end
+
+  it 'prints error message when table is full' do
+    script = (1..1401).map do |i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".die"
+    result = run_script(script)
+    expect(result[-2]).to eq('bdb > No more space left in table.')
+  end
 end
