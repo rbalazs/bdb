8f76deb48c62e4d090f6d89eef8edd835f74fce6
Richer Balázs <richer.balazs@arkon.hu>
2018-12-07 09:24:42 +0100

    For loop initial declarations are only allowed in C99 mode

--- execute_fncs.h
+++ execute_fncs.h
@@ -25,7 +25,8 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 
 ExecuteResult execute_select (Statement* statement, Table* table) {
   Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
+  uint32_t i = 0;
+  for (i = 0; i < table->num_rows; i++) {
     deserialize_row(row_slot(table, i), &row);
     print_row(&row);
   }
