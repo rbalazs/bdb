db77bb97cc920b1e5265fe43053b83dfaa1fa3fb
Balázs Richer <richer.balazs@gmail.com>
2018-12-15 14:05:01 +0100

    Renaming few headers for better overal stucture.

--- bdb.c
+++ bdb.c
@@ -8,7 +8,8 @@
 #include "enums.h"
 #include "row.h"
 #include "table.h"
-#include "execute_fncs.h"
+#include "execute_result.h"
+#include "prepare_result.h"
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
@@ -25,53 +26,6 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
   } else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
   }
-}
-
-PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
-  statement->type = STATEMENT_INSERT;
-
-  char* keyword = strtok(input_buffer->buffer, " ");
-  char* id_string = strtok(NULL, " ");
-  char* username = strtok(NULL, " ");
-  char* email = strtok(NULL, " ");
-
-  if (id_string == NULL || username == NULL || email == NULL) {
-    return PREPARE_SYNTAX_ERROR;
-  }
-
-  int id = atoi(id_string);
-
-  if (id < 0) {
-    return PREPARE_NEGATIVE_ID;
-  }
-
-  if (strlen(username) > COLUMN_USERNAME_SIZE) {
-    return PREPARE_STRING_TOO_LONG;
-  }
-
-  if (strlen(email) > COLUMN_EMAIL_SIZE) {
-    return PREPARE_STRING_TOO_LONG;
-  }
-
-  statement->row_to_insert.id = id;
-  strcpy(statement->row_to_insert.username, username);
-  strcpy(statement->row_to_insert.email, email);
-
-  return PREPARE_SUCCESS;
-}
-
-PrepareResult prepare_statement(InputBuffer* input_buffer,
-                                Statement* statement) {
-  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    return prepare_insert(input_buffer, statement);
-  }
-
-  if (strcmp(input_buffer->buffer, "select") == 0) {
-    statement->type = STATEMENT_SELECT;
-    return PREPARE_SUCCESS;
-  }
-
-  return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
 void print_prompt() { printf(">"); }
