f591bb4f25d47a6d4416fd1766e35f1aa7b967a9
Balázs Richer <richer.balazs@gmail.com>
2019-03-10 07:04:14 +0100

    Layout for leaf nodes.

--- btree.h
+++ btree.h
@@ -5,6 +5,9 @@ enum NodeType_t {
 
 typedef enum NodeType_t NodeType;
 
+/**
+ * Header layout.
+ */
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
 const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
@@ -12,3 +15,12 @@ const uint32_t IS_ROOT_OFFSET = sizeof(uint8_t);
 const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
 const uint32_t PARENT_POINTER_OFFSET = sizeof(uint8_t) + sizeof(uint8_t);
 const uint8_t COMMON_HEADER_SIZE = sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint32_t);
+
+/**
+ * Leaf layout.
+ */
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET =
+        sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint32_t);
+const uint32_t LEAF_NODE_HEADER_SIZE =
+        sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint32_t) + sizeof(uint32_t);
