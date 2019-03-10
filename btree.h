b7c360c0f375730491001766808b89f8918b6159
Balázs Richer <richer.balazs@gmail.com>
2019-03-10 07:00:30 +0100

    Header for node.

--- btree.h
+++ btree.h
@@ -4,3 +4,11 @@ enum NodeType_t {
 };
 
 typedef enum NodeType_t NodeType;
+
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = sizeof(uint8_t);
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = sizeof(uint8_t) + sizeof(uint8_t);
+const uint8_t COMMON_HEADER_SIZE = sizeof(uint8_t) + sizeof(uint8_t) + sizeof(uint32_t);
